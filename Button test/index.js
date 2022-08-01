cc4ec1b202edf09d31af1e7db4f51b741a07acbc
Minion <nessakristo@gmail.com>
2022-08-01 09:33:58 +0100

    playing with alert

---
+++ Button test/index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    alert("Hello World!");
+ };
+
+ function mySelectFunction() {
+    alert("line!");
+  }
+
+  function mySelectFunction2() {
+    alert("pie!");
+  }
+  
+//   // Alert some text when the text in the text field has been selected
+//   function myAlertFunction() {
+//     alert("You selected some text!");
+//   }
